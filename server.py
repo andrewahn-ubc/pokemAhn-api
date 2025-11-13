e23ee3aa0af42d5bc39396f09c103246713345f6
Andrew Ahn <andrewahn21@gmail.com>
2025-11-13 13:27:09 -0800

    set up generate_text endpoint

---
+++ server.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+
+app = FastAPI(title = "NPC Dialogue API for pokemAhn")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # or ["*"] for all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = pipeline("text-generation", "HuggingFaceTB/SmolLM2-1.7B-Instruct", temperature=0.7)
+
+
+class PromptRequest(BaseModel):
+    prompt: str
+
+@app.post("/")
+async def generate_text(request: PromptRequest):
+    prompt = request.prompt
+    result = model(
+        prompt,
+        max_new_tokens=20,    # limit response length
+        do_sample=True,       # enable stochastic sampling
+        top_p=0.9,            # nucleus sampling
+        temperature=0.5       # add creativity
+    )[0]["generated_text"]
+    return {"response": result}
+
